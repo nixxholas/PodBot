e625338d920d67ef5bb988a579cb3082fcdfe61f
nixxholas <nixholas@outlook.com>
2017-04-01 10:54:16 +0800

    app: Attempt 1: 500 Internal Server Error Offshore
    Signed-off-by: nixxholas <nixholas@outlook.com>

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var UserList = new Array();
 
 // Setup Restify Server
 const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
