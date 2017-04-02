3928b6424995bc390f4871b5473ae517dd42a505
nixxholas <nixholas@outlook.com>
2017-04-02 17:28:51 +0800

    Dialogs: RootDialog: Resolve Stoppage
    Signed-off-by: nixxholas <nixholas@outlook.com>

--- PodBotCSharp/Dialogs/RootDialog.cs
+++ PodBotCSharp/Dialogs/RootDialog.cs
@@ -9,11 +9,9 @@ namespace PodBotCSharp.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
+        public async Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
-
-            return Task.CompletedTask;
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
